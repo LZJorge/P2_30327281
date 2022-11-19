9fcf8f8eddabf180de08965401083badf5bee9d4
Jorge <jorgelandaeta33@gmail.com>
2022-11-19 16:26:26 -0400

    Entrega de asigncion

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+AOS.init({
+    duration: 1300,
+    once: true
+});
+
+const typing = document.getElementById('typing')
+
+let text = "<Desarrollador web>"
+
+let i = 0
+const typingAnimation = () => {
+
+    if (i < text.length) {
+        typing.innerHTML += text.charAt(i)
+        i++
+
+        setTimeout( typingAnimation, 150 )
+    } else {
+        setTimeout( clear, 4999 )
+        setTimeout( typingAnimation, 5000 )
+    }
+}
+
+const clear = () => {
+    i = 0
+    typing.innerHTML = ""
+}
+
+typingAnimation()
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Asignacion 1' });
+router.get('/', function (req, res) {
+    res.render('index.html');
 });
 
 module.exports = router;
